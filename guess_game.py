007863bd2169ef90337792ebae1bf59029d80fd0
DanGM96 <36743739+DanGM96@users.noreply.github.com>
2019-05-17 13:38:33 -0300

    versao 3: dando mais chances ao jogador (while)

--- guess_game.py
+++ guess_game.py
@@ -3,19 +3,29 @@ print("Bem vindo ao jogo de Adivinhação")
 print("******************************\n")
 
 secret_number = 42
-guess_str = input("Digite o seu número: ")
-guess = int(guess_str)
+tries = 3           # Três tentativas
+run = 1
 
-correct = guess == secret_number
-bigger = guess > secret_number
-smaller = guess < secret_number
+while tries:
+    print("\nTentativa", run, "de", tries)
 
-if correct:
-    print("Correto! A resposta pra vida, o universo e tudo mais!")
-else:
-    if bigger:
-        print("O número certo é MENOR do que esse. Estou decepcionado com você.")
-    elif smaller:
-        print("O número certo é MAIOR do que esse. Estou decepcionado com você.")
+    guess_str = input("Digite o seu número: ")
+    guess = int(guess_str)
 
-print("\nEndgame.")
+    correct = guess == secret_number
+    bigger = guess > secret_number
+    smaller = guess < secret_number
+
+    if correct:
+        print("Correto! A resposta pra vida, o universo e tudo mais!")
+    else:
+        if bigger:
+            print("O número certo é MENOR do que esse. Estou decepcionado com você.")
+        elif smaller:
+            print("O número certo é MAIOR do que esse. Estou decepcionado com você.")
+
+    run += 1
+    if run > 3:
+        tries = 0
+
+print("\nFim de jogo.")
