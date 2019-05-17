2beae3c7a575e1807adf05dec3220257e5838e70
DanGM96 <36743739+DanGM96@users.noreply.github.com>
2019-05-17 13:23:34 -0300

    versao 2: dando dicas ao jogador (elif)

--- guess_game.py
+++ guess_game.py
@@ -3,13 +3,19 @@ print("Bem vindo ao jogo de Adivinhação")
 print("******************************\n")
 
 secret_number = 42
+guess_str = input("Digite o seu número: ")
+guess = int(guess_str)
 
-guess = input("Digite o seu número: ")
-print("n = ", guess)
+correct = guess == secret_number
+bigger = guess > secret_number
+smaller = guess < secret_number
 
-if secret_number == int(guess):
-    print("A resposta pra vida, o universo e tudo mais!")
+if correct:
+    print("Correto! A resposta pra vida, o universo e tudo mais!")
 else:
-    print("Estou decepcionado com sua resposta.")
+    if bigger:
+        print("O número certo é MENOR do que esse. Estou decepcionado com você.")
+    elif smaller:
+        print("O número certo é MAIOR do que esse. Estou decepcionado com você.")
 
-
+print("\nEndgame.")
