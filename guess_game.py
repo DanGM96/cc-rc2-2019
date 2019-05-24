7cfd454a29cb43681a6f07a9151f9d02a6ad3168
DanGM96 <36743739+DanGM96@users.noreply.github.com>
2019-05-24 08:12:29 -0300

    versao 5: interrompendo o laço (break, continue)

--- guess_game.py
+++ guess_game.py
@@ -4,20 +4,23 @@ print("******************************\n")
 
 secret_number = 42
 tries = 3           # Três tentativas
-run = 1
 
 for run in range(tries):
     print("\nTentativa {} de {}".format(run + 1, tries))
-
-    guess_str = input("Digite o seu número: ")
+    guess_str = input("Chute um numero entre 1 e 100:")
     guess = int(guess_str)
 
     correct = guess == secret_number
     bigger = guess > secret_number
     smaller = guess < secret_number
 
+    if guess < 1 or guess > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     if correct:
         print("Correto! A resposta pra vida, o universo e tudo mais!")
+        break
     else:
         if bigger:
             print("O número certo é MENOR do que esse. Estou decepcionado com você.")
