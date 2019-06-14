0f74a36efad18683e170f6f050771eb302633d05
DanGM96 <jordangm@outlook.com>
2019-06-14 10:41:14 -0300

    PyGeoCode: Search1 refinado

--- 2_pygeocode/search1.py
+++ 2_pygeocode/search1.py
@@ -1,7 +1,7 @@
 from geopy.geocoders import Nominatim
 
 if __name__ == '__main__':
-    address = 'Ceará, Ituiutaba, MG'
+    address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
     user_agent = 'Search1'
     location = Nominatim(user_agent=user_agent).geocode(address, exactly_one=False) # addressLine, locality (city), adminDistrict (state), countryRegion, or postalcode
 
