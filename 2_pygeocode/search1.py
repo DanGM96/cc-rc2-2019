fc32aa59ac5eb1b05e973fd42ec2c47d7c896fa4
DanGM96 <jordangm@outlook.com>
2019-06-14 10:38:05 -0300

    PyGeoCode: Search1 refinado

--- 2_pygeocode/search1.py
+++ 2_pygeocode/search1.py
@@ -1,8 +1,15 @@
 from geopy.geocoders import Nominatim
 
-
 if __name__ == '__main__':
-    address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
+    address = 'Ceará, Ituiutaba, MG'
     user_agent = 'Search1'
-    location = Nominatim(user_agent=user_agent).geocode(address)
-    print(location.latitude, location.longitude)
+    location = Nominatim(user_agent=user_agent).geocode(address, exactly_one=False) # addressLine, locality (city), adminDistrict (state), countryRegion, or postalcode
+
+    print('Endereço buscado: ', address)
+    i = 0
+    for l in location:
+        aux = l[0].split(',') # lista
+        print('\tResultado ', i + 1, ': ')
+        print('\t\tCEP: ', aux[len(aux) - 2]) # pega a penúltima posição da lista
+        print(f'\t\t(Latitude, Longitude): ({l.latitude}, {l.longitude})')
+        i = i + 1
